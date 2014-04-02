2e8eede99ee567f0e261bce28676a32dc0426c48
ismael diawara <isamepro@hotmail.com>
2014-04-01 21:49:49 -0400

    Update String.cs

--- String.cs
+++ String.cs
@@ -1,1 +1,28 @@
-dd
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using System.Globalization;
+
+namespace IsameprO
+{
+    public static class StringExtension
+    {
+        /// <summary>
+        /// Check if string value is numeric
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static bool IsNumeric(this string value)
+        {
+            float resultat;
+            return float.TryParse(value, out resultat);
+        }
+        
+        
+      
+    }
+}
