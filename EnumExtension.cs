fe172248652c30c9478fccf054099d13cc45e31c
ismael diawara <isamepro@hotmail.com>
2014-04-01 21:53:37 -0400

    Update EnumExtension.cs

--- EnumExtension.cs
+++ EnumExtension.cs
@@ -1,1 +1,33 @@
-dd
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.ComponentModel;
+
+namespace Sage.Commun
+{
+    public static class EnumExtension
+    {
+        /// <summary>
+        /// Get the value of enum property [Description]
+        /// </summary>
+        public static string GetEnumDescription(this System.Enum value)
+        {
+            FieldInfo fi = value.GetType().GetField(value.ToString());
+
+            DescriptionAttribute[] attributes = (DescriptionAttribute[])fi.GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+            if (attributes != null && attributes.Length > 0)
+            {
+                return attributes[0].Description;
+            }
+            else
+            {
+                return value.ToString();
+            }
+        }
+
+    }
+}
+
